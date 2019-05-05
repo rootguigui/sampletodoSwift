02abf3ccc7a8012a7813992bda7350d9e917e104
gui <gui@guis-imac.local>
2019-05-05 15:19:43 -0300

    Initial Commit

---
+++ todo_list/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  todo_list
+//
+//  Created by gui on 05/05/19.
+//  Copyright © 2019 gui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
